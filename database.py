0d55f9d3388cdb7dff4f942e69464200c39e1f0f
JustQerq <theqerq@gmail.com>
2026-06-07 19:09:28 +0400

    Implemented basic database for storing scraped data from pages

---
+++ database.py
@@ -0,0 +1,127 @@
+import sqlite3
+import json
+
+
+def init_database():
+    """
+    Initializes the database and creates tables if not present
+    """
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS rent_raw (
+                id INTEGER PRIMARY KEY,
+                title TEXT,
+                href TEXT,
+                href_visited INTEGER,
+                price REAL,
+                city TEXT,
+                district TEXT,
+                microdistrict TEXT,
+                street TEXT,
+                floor TEXT,
+                max_floors TEXT,
+                area REAL,
+                rooms REAL,
+                description TEXT,
+                advertiser TEXT,
+                construction_type TEXT,
+                is_furnished INTEGER,
+                heating TEXT,
+                payment_type TEXT,
+                extra_info TEXT,
+                other TEXT
+            );
+        """)
+
+
+def get_rent_ids():
+    """
+    Returns a set of unique ad ids stored in the database
+
+    Returns:
+        set[int]: set of unique ad ids
+    """
+    with sqlite3.connect("data.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT id FROM rent_raw")
+        result = cur.fetchall()
+        return set[int]([row["id"] for row in result])
+
+
+def save_rent_items(items: list[dict]):
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        rows = []
+        for item in items:
+            extra_info = item.get("extra_info")
+            if isinstance(extra_info, list):
+                extra_info = ", ".join(extra_info)
+            
+            other = item.get("other")
+            if isinstance(other, list):
+                other = ", ".join(other)
+
+            href_visited = item.get("href_visited", False)
+            is_furnished = item.get("is_furnished")
+            if isinstance(is_furnished, bool):
+                is_furnished = int(is_furnished)
+            
+            price = item.get("price")
+            if isinstance(price, str):
+                try:
+                    price = float(price)
+                except:
+                    price = None
+            
+            area = item.get("area")
+            if isinstance(area, str):
+                try:
+                    area = float(area)
+                except:
+                    area = None
+            
+            rooms = item.get("rooms")
+            if isinstance(rooms, str):
+                try:
+                    rooms = float(rooms)
+                except:
+                    rooms = None
+
+            rows.append((
+                int(item["data-id"]),
+                item.get("title"),
+                item.get("href"),
+                int(href_visited),
+                price,
+                item.get("city"),
+                item.get("district"),
+                item.get("microdistrict"),
+                item.get("street"),
+                item.get("floor"),
+                item.get("max_floors"),
+                item.get("area"),
+                item.get("rooms"),
+                item.get("description"),
+                item.get("advertiser"),
+                item.get("construction_type"),
+                is_furnished,
+                item.get("heating"),
+                item.get("payment_type"),
+                extra_info,
+                other,
+            ))
+
+        cur.executemany("""
+            INSERT INTO rent_raw (
+                id, title, href, href_visited, price,
+                city, district, microdistrict, street,
+                floor, max_floors, area, rooms, description,
+                advertiser, construction_type, is_furnished,
+                heating, payment_type, extra_info, other
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT (id)
+            DO NOTHING;
+        """, rows)
+        con.commit()
